f9b196cf66fb6f64945daf10ccac8f3119613872
sandrusmb <sandrusmb@gmail.com>
2019-12-20 08:30:33 +0100

    Add docs folder

---
+++ docs/precache-manifest.77f2a77b5db20231f24e7552c0ece535.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8a13418f2227d54cdaf98c8cc030fce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c4446280963299b50024",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "dc5d439e3845155f6228",
+    "url": "./static/js/2.ea74814c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.ea74814c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4446280963299b50024",
+    "url": "./static/js/main.70c43d9e.chunk.js"
+  },
+  {
+    "revision": "a9eb3b777736d22f046d",
+    "url": "./static/js/runtime-main.e99222d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
